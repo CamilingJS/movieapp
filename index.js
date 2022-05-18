8d2cca258119e3a1779b5ce245f4bd8ea50139fe
CamilingJS <jonathancamiling@gmail.com>
2022-05-17 23:21:50 -0700

    to create movie summary element onOptionSelect, we moved and provided this method to each createAutoComplete method to display left-summary and right-summary elements

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const autoCompleteConfig = {
         ${movie.Title} (${movie.Year})
         `;
     }, 
-    onOptionSelect(movie) {
-        document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie);
-    }, 
+    
     inputValue(movie){
         return movie.Title; 
     }, 
@@ -30,15 +27,23 @@ const autoCompleteConfig = {
 
 createAutoComplete({
     ...autoCompleteConfig,
-    root: document.querySelector('#left-autocomplete')
+    root: document.querySelector('#left-autocomplete'), 
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie, document.querySelector('#left-summary'));
+    }, 
 });
 createAutoComplete({
     ...autoCompleteConfig,
-    root: document.querySelector('#right-autocomplete')
+    root: document.querySelector('#right-autocomplete'), 
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie, document.querySelector('#right-summary'));
+    }, 
 });
 
 
-const onMovieSelect = async movie => {
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '5be6879',
@@ -46,7 +51,7 @@ const onMovieSelect = async movie => {
         }
     });
     
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+    summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
