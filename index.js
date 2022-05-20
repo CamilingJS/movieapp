7f881c46c15d21ca20b2dea22be2cdbd7b4a6c3d
CamilingJS <jonathancamiling@gmail.com>
2022-05-19 19:35:47 -0700

    added movie plot and actors detials

--- index.js
+++ index.js
@@ -142,6 +142,14 @@ const autoCompleteConfig = {
         <p class="title">${movieDetail.imdbVotes}</p>
         <p class="subtitle">IMDB Votes</p>
       </article>
+      <article data-value=${imdbRating} class="notification is-primary">
+        <p class="title">${movieDetail.Plot}</p>
+        <p class="subtitle">Plot</p>
+      </article>
+      <article data-value=${dollars} class="notification is-primary">
+        <p class="title">${movieDetail.Actors}</p>
+        <p class="subtitle">Actors</p>
+      </article>
     `;
   };
   
